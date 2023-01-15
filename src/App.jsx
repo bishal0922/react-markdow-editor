69c129edb82e429efb5aa81086dfd9dc3dcbc0ff
Bishal Giri <bishal0922@gmail.com>
2023-01-14 20:38:05 -0600

    add textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import ReactMarkdown from 'react-markdown';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [input, setInput] = useState()
+  //for handling chanages in the main textarea
+  const handleTextAreaChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const inputa = '# This is a header\n\nAnd this is a paragraph'
+
 
   return (
     <div className="App">
-      Hello World
+      <textarea autoFocus className="textarea" value={input} onChange={handleTextAreaChange}/>
+      {/* <ReactMarkdown className="markdown" source={inputa} ></ReactMarkdown> */}
+      ReactDOM.render(<ReactMarkdown source={inputa} />, document.getElementById('container'))
     </div>
   )
 }
